4a3da94803bc1f7a12881039004954636c909327
awardfact <60775999+awardfact@users.noreply.github.com>
2022-07-28 16:54:17 +0900

    Update sangpage.js

--- sangpage.js
+++ sangpage.js
@@ -4,20 +4,9 @@ const http = require('http');
 const app = express();
 const fs = require('fs');
 
-const options = { // letsencrypt로 받은 인증서 경로를 입력해 줍니다.
 
-    ca: fs.readFileSync('/etc/letsencrypt/live/sangpage.com/fullchain.pem'),
+http.createServer(app).listen(8080);
 
-    key: fs.readFileSync('/etc/letsencrypt/live/sangpage.com/privkey.pem'),
-
-    cert: fs.readFileSync('/etc/letsencrypt/live/sangpage.com/cert.pem')
-
-    };
-
-
-
-http.createServer(app).listen(4000);
-http.createServer(options,app).listen(443)
 //app.set('port' , process.env.PORT || 4000);
 app.get('/' , (req,res) =>{
     res.send('익스프레스 시작');
